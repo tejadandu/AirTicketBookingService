2f225495486e6d7f7b7a97243feb793dbd3b83c8
Teja Dandu <teja.dandu484@gmail.com>
2022-12-29 10:57:06 +0530

    Added error files

---
+++ src/utils/errors/service-error.js
@@ -0,0 +1,20 @@
+const { StatusCodes } = require('http-status-codes');
+
+
+class ServiceError extends Error {
+    constructor(
+        message = "Something went wrong",
+        explanation = "Service layter error",
+        statusCode = INTERNAL_SERVER_ERROR
+    ){
+        super();
+        this.name = "ServiceError";
+        this.message = message;
+        this.explanation = explanation;
+        this.statusCode = statusCode;
+
+    }
+}
+
+
+module.exports = ServiceError;
